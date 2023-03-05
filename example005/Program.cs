de96d999cd9dea3687fd6598bf9010a9fe920f36
Henn <hennogyrchik@gmail.com>
2023-03-05 12:55:00 +0300

    example005

---
+++ example005/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите и мя пользователя: ");
+string username=Console.ReadLine();
+if (username.ToLower()=="маша") {
+    Console.WriteLine("Ура! Это Маша!");
+}else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
